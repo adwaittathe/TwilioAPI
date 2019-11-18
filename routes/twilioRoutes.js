2c1d16a63c3a8692c956137c4a232e27cd6237bc
Adwait Tathe <adwaittathe@Adwaits-MacBook-Pro.local>
2019-11-18 04:51:57 -0500

    Symptom 6

--- routes/twilioRoutes.js
+++ routes/twilioRoutes.js
@@ -10,94 +10,110 @@ router.post('/register' , async (req,res) => {
     let from =  req.body.From;
     let to = req.body.To;
     let msgBody = req.body.Body; 
+    console.log("FROM " + from);
+    console.log("TO " + to);
+    console.log("BODY " + msgBody);
     let phone = await phoneModel.findOne({phoneNo : from });
-    if(msgBody == "START")
-    {     
-        if(!phone){
-            let phoneObj = new phoneModel({
-                phoneNo : from
-            })
-            phone = await phoneObj.save();
-            await client.messages.create({
-              to : from,
-              from : process.env.TWILIO_PHONE_NO,
-              body : 'Welcome to the study',})
-        }
-        if(!phone.symptom){
-            await client.messages.create({
-                to : from,
-                from : process.env.TWILIO_PHONE_NO,
-                body : 'Please indicate your symptom (1)Headache, (2)Dizziness, (3)Nausea, (4)Fatigue, (5)Sadness, (0)None'})
-        }
-    }
-    else{
     if(phone)
     {
-        if(!phone.symptom){
-            let symptomList = [];
-            let sympObj   
-            switch(msgBody){
-                case "1":
-                    sympObj = "Headache";
-                    break;
-                
-                case "2":
-                    sympObj = "Dizziness";
-                    break;
-            
-                case "3":
-                    sympObj = "Nausea";
-                    break;
-    
-                case "4":
-                    sympObj = "Fatigue";
-                    break;
-                    
-                case "5":
-                    sympObj = "Sadness";
-                    break;
-    
-                case "0":
-                    sympObj = "None";
-                    break;
-    
-                default:
-                    sympObj = "wrongInput";            
-            }
-            if(sympObj == "wrongInput")
-            {
-                await client.messages.create({
-                    to : from,
-                    from : process.env.TWILIO_PHONE_NO,
-                    body : 'Please enter a number from 0 to 5'})
-            }
-            else if(sympObj == "None")
-            {
-                await client.messages.create({
-                    to : from,
-                    from : process.env.TWILIO_PHONE_NO,
-                    body : 'Thank you and we will check with you later.'})
-            }
-            else{
-                symptomList.push(sympObj);
-                console.log("msgBody  " + msgBody);
-                await phoneModel.findOneAndUpdate({phoneNo : from},
-                {
-                    $set:{
-                        symptom : symptomList
-                    }
-                });
-                await client.messages.create({
-                    to : from,
-                    from : process.env.TWILIO_PHONE_NO,
-                    body : "On a scale from 0 (none) to 4 (severe), how would you rate your "+ sympObj +" in the last 24 hours?"})
-            }
-    
-        }
-
-    } 
-
+        res.send({
+            "result" : "yes"
+        })
     }
+    else
+    {
+        res.send({
+            "result" : "No"
+        })
+    }
+
+    // if(msgBody == "START")
+    // {     
+    //     if(!phone){
+    //         let phoneObj = new phoneModel({
+    //             phoneNo : from
+    //         })
+    //         phone = await phoneObj.save();
+    //         await client.messages.create({
+    //           to : from,
+    //           from : process.env.TWILIO_PHONE_NO,
+    //           body : 'Welcome to the study',})
+    //     }
+    //     if(!phone.symptom){
+    //         await client.messages.create({
+    //             to : from,
+    //             from : process.env.TWILIO_PHONE_NO,
+    //             body : 'Please indicate your symptom (1)Headache, (2)Dizziness, (3)Nausea, (4)Fatigue, (5)Sadness, (0)None'})
+    //     }
+    // }
+    // else{
+    // if(phone)
+    // {
+    //     if(!phone.symptom){
+    //         let symptomList = [];
+    //         let sympObj   
+    //         switch(msgBody){
+    //             case "1":
+    //                 sympObj = "Headache";
+    //                 break;
+                
+    //             case "2":
+    //                 sympObj = "Dizziness";
+    //                 break;
+            
+    //             case "3":
+    //                 sympObj = "Nausea";
+    //                 break;
+    
+    //             case "4":
+    //                 sympObj = "Fatigue";
+    //                 break;
+                    
+    //             case "5":
+    //                 sympObj = "Sadness";
+    //                 break;
+    
+    //             case "0":
+    //                 sympObj = "None";
+    //                 break;
+    
+    //             default:
+    //                 sympObj = "wrongInput";            
+    //         }
+    //         if(sympObj == "wrongInput")
+    //         {
+    //             await client.messages.create({
+    //                 to : from,
+    //                 from : process.env.TWILIO_PHONE_NO,
+    //                 body : 'Please enter a number from 0 to 5'})
+    //         }
+    //         else if(sympObj == "None")
+    //         {
+    //             await client.messages.create({
+    //                 to : from,
+    //                 from : process.env.TWILIO_PHONE_NO,
+    //                 body : 'Thank you and we will check with you later.'})
+    //         }
+    //         else{
+    //             symptomList.push(sympObj);
+    //             console.log("msgBody  " + msgBody);
+    //             await phoneModel.findOneAndUpdate({phoneNo : from},
+    //             {
+    //                 $set:{
+    //                     symptom : symptomList
+    //                 }
+    //             });
+    //             await client.messages.create({
+    //                 to : from,
+    //                 from : process.env.TWILIO_PHONE_NO,
+    //                 body : "On a scale from 0 (none) to 4 (severe), how would you rate your "+ sympObj +" in the last 24 hours?"})
+    //         }
+    
+    //     }
+
+    // } 
+
+    // }
          
 });
 
